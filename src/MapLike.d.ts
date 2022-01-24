de4338ec5526af2812f22afebed4982abca86c1d
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2022-01-24 11:51:53 +0900

    Update MapLike.d.ts

--- src/MapLike.d.ts
+++ src/MapLike.d.ts
@@ -1,9 +1,13 @@
 export default interface MapLike<K, V> {
+    get size(): number;
+
     has(key: K): boolean;
     get(key: K): V;
     set(key: K, value: V): void;
     delete(key: K): void;
     clear(): void;
+
+    forEach(callbackFn: (value: V, key: K, map: MapLike<K, V>) => void): void;
 
     keys(): Iterable<K>;
     values(): Iterable<V>;
